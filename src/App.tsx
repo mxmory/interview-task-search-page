939f5419c72ba1b6a328306db23fc25c0d10d3f4
Aidar <a.khisamutdinov@sbereducation.ru>
2023-11-17 11:58:36 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+   {/* тут писать код */}
+
+  return (
+    <div>
+      <h3>Search</h3>
+      <input
+        placeholder="Product name..."
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/api.ts
@@ -0,0 +1,2 @@
+const MY_SEARCH_URL = 'https://dummyjson.com/products/search?q=';
+
